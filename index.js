61715e6a801bf397fbaec8d4ae91e15cba705ed8
elfacu0 <30785449+elfacu0@users.noreply.github.com>
2020-05-03 15:53:48 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 const cors = require('cors');
 require('dotenv/config');
 
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -16,7 +17,6 @@ app.get('/*', (req, res) => {
 });
 
 const homeRoute = require('./routes/home');
-app.use(cors());
 app.use('/', homeRoute);
 
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () => {
