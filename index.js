4c91ef7a977ef3792691d5d9de4abd29a3c95ee9
elfacu0 <30785449+elfacu0@users.noreply.github.com>
2020-05-03 15:30:34 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const mongoose = require('mongoose');
+const port = process.env.PORT || 5000;
+const cors = require('cors');
+require('dotenv/config');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+const homeRoute = require('./routes/home');
+app.use(cors());
+app.use('/', homeRoute);
+
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () => {
+    console.log('connecting');
+    console.log(mongoose.connection.readyState);
+    console.log(
+        mongoose.connection.readyState === 1
+            ? 'CONNECTION SUCCESSFUL'
+            : 'ERROR IN CONNECTION'
+    );
+});
+
+app.listen(port);
+console.log('App is listening on port ' + port);
